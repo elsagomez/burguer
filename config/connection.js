00fef42ea839480fe151f77ce73d7542ec1c564f
elsagomez <elsa.ghdz@gmail.com>
2017-05-16 00:36:05 -0400

    Add Jaws, ready to deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,22 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+	connection = mysql.createConnection({
   host     : "127.0.0.1",
   port     : 3306,
   user    : 'root',
   password : 'Stpvwhxi8247!',
   database: "burgerApp_db"
-});
 
-connection.connect(function(err){
-	if(err){
-		console.log("error connection: " + err.stack);
-		return;
-	}
-	console.log("connected as id: " + connection.threadId);
-});
+	});
+
+};
+
+connection.connect();
 
 module.exports = connection;
